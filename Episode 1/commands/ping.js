f7d76eef305f99f35a202ee0ca047dfc2529f2db
vyrenx <68992864+vyrenx@users.noreply.github.com>
2021-01-29 20:28:09 +0800

    c

--- Episode 1/commands/ping.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-unused-vars */
-const ms = require('ms');
-exports.run = async (client, message, args) => {
-	const m = await message.channel.send('Pinging...');
-	m.edit(`Latency is \`${m.createdTimestamp - message.createdTimestamp}\`, API latency is \`${client.ws.ping}\`, Uptime is \`${ms(Math.ceil(client.uptime), { long: true })}\``);
-};
